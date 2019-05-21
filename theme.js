b07b0ca094dc69651f4b6bd8581ab07ade22aa6a
Magnus Helmersson <magnus.helmersson@universalavenue.com>
2019-05-21 11:51:40 +0200

    Initial commit

---
+++ theme.js
@@ -0,0 +1,9 @@
+import { dark } from 'mdx-deck/themes'
+import okaidia from 'react-syntax-highlighter/dist/styles/prism/duotone-space'
+
+export default {
+  ...dark,
+  prism: {
+    style: okaidia
+  }
+}
